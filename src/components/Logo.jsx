2aae43193212e3a50950495031beece0a5462877
jannis2301 <jannis2301@users.noreply.github.com>
2023-11-18 14:58:21 +0000

    deploy: dcbb66f6c596d4d224d8309e64a2a0a5b8ac354a

--- src/components/Logo.jsx
+++
@@ -1,4 +0,0 @@
-const Logo = () => {
-  return <h2>MS was geht?</h2>
-}
-export default Logo
--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import Home from './Home'
-import About from './About'
-import Contact from './Contact'
-import Landing from './Landing'
-import Register from './Register'
-import Error from './Error'
-import Clubs from './Clubs'
-
-export { Home, About, Landing, Register, Error, Clubs, Contact }
